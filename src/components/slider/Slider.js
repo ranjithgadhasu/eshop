442c4d14f753e26057e52d6d2ea824e8ed5c1e54
Ranjith Gadhasu <ranjithgadhasu@gmail.com>
2023-08-26 18:48:53 +0530

    ranji

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -1,66 +1,70 @@
-import{ useEffect, useState } from 'react'; 
+import { useEffect, useState, useRef, useCallback } from "react";
 import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
-import { sliderData } from './slider.data';
+import { sliderData } from "./slider.data";
 import "./Slider.scss";
 
-
 const Slider = () => {
-    const [currentSlide, setCurrentSlide] = useState(0);
-    const slideLength = sliderData.length;
-       console.log(slideLength);
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const slideLength = sliderData.length;
 
+  const autoScroll = true;
+  const intervalTime = 5000;
 
-    const autoScroll = true
-    let slideInterval;
-    let intervalTime = 5000;
+  const nextSlide = useCallback(() => {  // Wrap nextSlide in useCallback
+    setCurrentSlide(currentSlide === slideLength - 1 ? 0 : currentSlide + 1);
+  }, [currentSlide, slideLength]);  // List dependencies of nextSlide
 
-    const nextSlide = () => {
-     setCurrentSlide(currentSlide === slideLength -1 ? 0: currentSlide + 1);
-    };
-    const prevSlide = () => {
-        setCurrentSlide(currentSlide === 0 ? slideLength -1 : currentSlide -1);
-    };
-    useEffect(() =>{
-       setCurrentSlide(0);
-    }, []);
+  const prevSlide = useCallback(() => {  // Also wrap prevSlide in useCallback
+    setCurrentSlide(currentSlide === 0 ? slideLength - 1 : currentSlide - 1);
+  }, [currentSlide, slideLength]);
+
+  useEffect(() => {
+    setCurrentSlide(0);
+  }, []);
+  
+  const slideIntervalRef = useRef();
+
+  useEffect(() => {
     function auto() {
-       slideInterval = setInterval(nextSlide, intervalTime);
-    };
-    useEffect(() => {
-      if (autoScroll) {
-        
-      };
-      auto();
-
-      return () => clearInterval(slideInterval);
-    }, [currentSlide, slideInterval, autoScroll]);
+      slideIntervalRef.current = setInterval(nextSlide, intervalTime);
+    }
     
-  return ( <div className="slider">
-     <AiOutlineArrowLeft className="arrow prev"  onClick={prevSlide} />
-     <AiOutlineArrowRight className="arrow next" onClick={nextSlide} />
+    if (autoScroll) {
+      auto();
+    }
 
-     {sliderData.map((Slide, index) => {
-        const {image, heading, desc } = Slide
+    return () => clearInterval(slideIntervalRef.current);
+  }, [currentSlide, intervalTime, autoScroll, nextSlide]);
+
+  return (
+    <div className="slider">
+      <AiOutlineArrowLeft className="arrow prev" onClick={prevSlide} />
+      <AiOutlineArrowRight className="arrow next" onClick={nextSlide} />
+
+      {sliderData.map((Slide, index) => {
+        const { image, heading, desc } = Slide;
         return (
-        <div key={index} className={index === currentSlide ? "slide current" : "slide"}>
-        
-           {index === currentSlide && (
-            <>
-            <img src={image} alt="slide"/>
-            <div className="content">
-                <h2>{heading}</h2>
-                <p>{desc}</p>
-                <hr/>
-                <a href="#product"  className="--btn --btn-primary">
+          <div
+            key={index}
+            className={index === currentSlide ? "slide current" : "slide"}
+          >
+            {index === currentSlide && (
+              <>
+                <img src={image} alt="slide" />
+                <div className="content">
+                  <h2>{heading}</h2>
+                  <p>{desc}</p>
+                  <hr />
+                  <a href="#product" className="--btn --btn-primary">
                     Shop Now
-                </a>
-            </div>
-            </>
-           )}
-            </div>
-        )
-     })}
-  </div>
+                  </a>
+                </div>
+              </>
+            )}
+          </div>
+        );
+      })}
+    </div>
   );
 };
 
